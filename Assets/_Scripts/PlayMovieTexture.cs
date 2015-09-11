56ce826fa6a3b26bb13a659826418b95185bc661
Joshua <jayseaLEARN@hotmail.com>
2015-09-11 19:13:57 +1200

    Add in all source files from the Unity project's "Source" file
    Template for commit message: http://udacity.github.io/git-styleguide/
    ---------------------------------------------------------------------
    
    feat: Summarize changes in around 50 characters or less
    
    More detailed explanatory text, if necessary. Wrap it to about 72
    characters or so. In some contexts, the first line is treated as the
    subject of the commit and the rest of the text as the body. The
    blank line separating the summary from the body is critical (unless
    you omit the body entirely); various tools like `log`, `shortlog`
    and `rebase` can get confused if you run the two together.
    
    Explain the problem that this commit is solving. Focus on why you
    are making this change as opposed to how (the code explains that).
    Are there side effects or other unintuitive consequenses of this
    change? Here's the place to explain them.
    
    Further paragraphs come after blank lines.
    
     - Bullet points are okay, too
    
     - Typically a hyphen or asterisk is used for the bullet, preceded
       by a single space, with blank lines in between, but conventions
       vary here
    
    If you use an issue tracker, put references to them at the bottom,
    like this:
    
    Resolves: #123
    See also: #456, #789

---
+++ Assets/_Scripts/PlayMovieTexture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovieTexture : MonoBehaviour {
+
+	public GameObject screen;
+
+	// Use this for initialization
+	void Start () {
+		// this line of code will make the Movie Texture begin playing
+		((MovieTexture)screen.GetComponent<Renderer>().material.mainTexture).Play ();
+	}
+
+}
